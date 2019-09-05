c5e79cebc0bc4d076828bed85a67a50d3a2338ef
Giovani Mantovani <giovani.mantovani@agrotools.com.br>
2019-09-05 17:39:39 -0300

    Atualizacao da autenticacao jwt

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserCotroller from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
@@ -7,5 +9,8 @@ const routes = new Router();
 
 routes.post('/users', UserCotroller.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserCotroller.update);
 
 export default routes;
